9984ccf75803cde6066f2e4c5c7702b2acc9458b
Edward Mizuka <mizuka.wu@outlook.com>
2026-09-13 16:49:40 +0800

    fix: snat_subnet default to true for subnet routing

--- scripts/libs/get_config.sh
+++ scripts/libs/get_config.sh
@@ -17,7 +17,7 @@ LOCK_FILE="/tmp/ts_monitor.lock"
 [ -z "$ts_version" ] && ts_version="1.102.4"
 [ -z "$arch" ] && arch="arm64"
 [ -z "$accept_dns" ] && accept_dns="false"
-[ -z "$snat_subnet" ] && snat_subnet="false"
+[ -z "$snat_subnet" ] && snat_subnet="true"
 [ -z "$hostname" ] && hostname="$(uname -n)"
 
 # 自动检测 tun 支持
